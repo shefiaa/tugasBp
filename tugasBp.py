3b8d57e236e3f2e11e9406de518cb9e4b6636f13
root <root@LAPTOP-2POVRUHJ.localdomain>
2022-10-03 21:32:05 +0700

    first commit

--- tugasBp.py
+++ tugasBp.py
@@ -1,2 +1,18 @@
+#Menampilkan Nama dan Nim
 print("Nama : Shefia Anggraeni")
 print("Nim : 20210801104")
+
+#program operator + - x
+x = 25
+y = 5
+print('x + y =', x+y)
+print('x - y =', x-y)
+print('x * y =', x*y)
+print('x / y =', x/y)
+#menampilkan matriks 2x2
+A = [[4, 5]]
+B = [[6, 3]]
+print('Matriks: ')
+print('[',A[0][0], ',', B[0][1], ']')
+print('[',A[0][1], ',', B[0][0], ']')
+
